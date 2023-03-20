765f971a0ddbbe2b2e7ca11f02a18b648d840dbf
EnhancedTarget <levente.kalman922@gmail.com>
2023-03-20 10:39:41 +0000

    main

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,46 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'ulster',
+    loadChildren: () => import('./ulster/ulster.module').then( m => m.UlsterPageModule)
+  },
+  {
+    path: 'munster',
+    loadChildren: () => import('./munster/munster.module').then( m => m.MunsterPageModule)
+  },
+  {
+    path: 'leinster',
+    loadChildren: () => import('./leinster/leinster.module').then( m => m.LeinsterPageModule)
+  },
+  {
+    path: 'connaught',
+    loadChildren: () => import('./connaught/connaught.module').then( m => m.ConnaughtPageModule)
+  },
+  {
+    path: 'ulster-counties',
+    loadChildren: () => import('./ulster-counties/ulster-counties.module').then( m => m.UlsterCountiesPageModule)
+  },
+  {
+    path: 'leinster-counties',
+    loadChildren: () => import('./leinster-counties/leinster-counties.module').then( m => m.LeinsterCountiesPageModule)
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
